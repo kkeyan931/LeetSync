adab4cf36984953df10d3f6f4bf3a53b53454187
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2024-11-13 21:28:40 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (35.64%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,15 +1,32 @@
 class Solution {
 public:
-     
-    int recHelper(int n, vector<int> &table) {
-        if(n <= 2) return table[n-1] = n;
+    int recHelper(int n, vector<int>& table) {
+        if (n <= 2)
+            return table[n - 1] = n;
 
-        if(table[n-1] != 0 ) return table[n-1];
+        if (table[n - 1] != 0)
+            return table[n - 1];
 
-        return table[n-1] = recHelper(n-1, table) + recHelper(n-2, table);
+        return table[n - 1] = recHelper(n - 1, table) + recHelper(n - 2, table);
+    }
+    int twoVarHelper(int n) {
+
+        if(n <= 2) return n;
+        int stepOne = 1;
+        int stepTwo = 2;
+
+        int result = 0;
+        for(int it=3; it<=n; it++) {
+
+            result = stepOne + stepTwo;
+            stepOne = stepTwo;
+            stepTwo = result;
+        }
+        return result;
     }
     int climbStairs(int n) {
         vector<int> table(n, 0);
-        return recHelper(n, table);
+        // return recHelper(n, table);
+        return twoVarHelper(n);
     }
 };
