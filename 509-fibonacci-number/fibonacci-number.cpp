a0487a4b7ea774c53498793ce47ee8bdd236147f
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-01-11 13:46:47 +0530

    Time: 2 ms (77.79%) | Memory: 7.7 MB (50.15%) - LeetSync

--- 509-fibonacci-number/fibonacci-number.cpp
+++ 509-fibonacci-number/fibonacci-number.cpp
@@ -9,8 +9,25 @@ public:
 
         return mem[n] = rec(n-1, mem) + rec(n-2, mem);
     }
+    int it(int n) {
+
+        if(n == 0) return 0;
+        
+        int a = 0, b = 1;
+
+        int pos = 2;
+        while(pos<=n) {
+            int temp = b;
+            b = a + b;
+            a = temp;
+            pos++;
+        }
+        return b;
+    }
     int fib(int n) {
-        vector<int> mem(n+1, 0);
-        return rec(n, mem);
+        // vector<int> mem(n+1, 0);
+        // return rec(n, mem);
+
+        return it(n);
     }
 };
