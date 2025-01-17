2787d867ad0646aaf811cba24be7ae5db7b01966
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-01-17 10:15:27 +0530

    Time: 2 ms (32.52%) | Memory: 97.4 MB (53.42%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,33 +1,16 @@
 class Solution {
 public:
-
-    int bruteForce(vector<int>& prices) {
-
-        int n = prices.size();
-        int max = 0;
-        for(int i=0;i<n;++i) {
-            for(int j=i+1;j<n;++j) {
-                if((prices[j] - prices[i]) > max) max = prices[j] - prices[i];
-            }
-        }
-        return max;
-    }
-    int kadaneAlgo(vector<int>& prices) {
-        int buy = prices[0];
-
-        int profit = 0;
-
-        for(int index = 1; index < prices.size(); ++index) {
-            if(prices[index] < buy) {
-                buy = prices[index];
-            } else if((prices[index] - buy) > profit) {
-                profit = prices[index] - buy;
-            }
-        }
-        return profit;
-    }
     int maxProfit(vector<int>& prices) {
-        // return bruteForce(prices);
-        return kadaneAlgo(prices);
+
+        int max = 0;
+        int min = prices[0];
+        for(int i=1;i<prices.size();++i) {
+            if(prices[i]-min > max) {
+                max = prices[i]-min;
+            }
+            if(prices[i]<min) min = prices[i];
+        }
+
+        return max;
     }
 };
