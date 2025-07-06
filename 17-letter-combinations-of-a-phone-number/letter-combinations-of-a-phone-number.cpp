9065d72a41d6900407caf51d6be547716614cd9e
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-07-06 18:42:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (99.88%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void rec(vector<string>& result, unordered_map<char, string>& umap,
+             string& temp, string& digits, int index) {
+
+        if (temp.size() == digits.size()) {
+            result.push_back(temp);
+            return;
+        }
+
+        for (int i = index; i < digits.size(); ++i) {
+            for (auto ch : umap[digits[i]]) {
+                temp.push_back(ch);
+                rec(result, umap, temp, digits, i + 1);
+                temp.pop_back();
+            }
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        if(digits.size() == 0) {
+            return {};
+        }
+        unordered_map<char, string> umap = {
+            {'2', "abc"}, {'3', "def"},  {'4', "ghi"}, {'5', "jkl"},
+            {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}};
+        vector<string> result;
+        string temp;
+        rec(result, umap, temp, digits, 0);
+        return result;
+    }
+};
