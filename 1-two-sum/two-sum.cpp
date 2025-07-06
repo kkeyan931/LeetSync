03429bb38970cb5b376e0006f4c7899e30b3e32d
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-07-06 08:51:56 +0530

    Time: 1 ms (80.21%) | Memory: 14.7 MB (58.65%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,14 +1,31 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> um;
+    vector<int> twoPointer(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<pair<int,int>> vec(n);
 
-        for(int i=0;i<nums.size();++i) {
-            if(um.find(target-nums[i])!=um.end()) {
-                return {i, um[target-nums[i]]};
+        for(int i = 0; i < n; ++i) {
+            vec[i] = {nums[i], i};
+        }
+        sort(vec.begin(), vec.end());
+
+        int ptr1 = 0;
+        int ptr2 = n - 1;
+
+        while(ptr1 < ptr2) {
+            int sum = vec[ptr1].first + vec[ptr2].first;
+            if(sum == target) {
+                return {vec[ptr1].second, vec[ptr2].second};
             }
-            um[nums[i]] = i;
+            if(sum < target) {
+                ptr1++;
+            } else {
+                ptr2--;
+            }
         }
         return {};
+    }
+    vector<int> twoSum(vector<int>& nums, int target) {
+        return twoPointer(nums, target);
     }
 };
