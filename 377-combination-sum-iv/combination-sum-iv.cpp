fcc70f4ff6ad2e87da48e50d4e4e88cee0d505ac
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-07-08 21:41:12 +0530

    Time: 4 ms (30.92%) | Memory: 9.4 MB (21.00%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int rec(vector<int>& nums, vector<vector<int>>& dp, int index, int target) {
-        
         if (target == 0) {
             return 1;
         }
@@ -18,8 +17,41 @@ public:
                    rec(nums, dp, 0, target - nums[index]) +
                    rec(nums, dp, index + 1, target);
     }
+    int rec2(vector<int>& nums, int target, vector<int>& dp) {
+        if (target == 0) return 1;       // Base case: One valid way (empty combination)
+        if (target < 0) return 0;        // Overshoot, invalid
+        if (dp[target] != -1) return dp[target];  // Memoized result
+
+        int count = 0;
+        for (int num : nums) {
+            count += rec2(nums, target - num, dp); // Pick num and recurse
+        }
+        return dp[target] = count;  // Save result
+    }
+
+    int mem(vector<int>& nums, int target) {
+        // vector<vector<int>> dp(nums.size(), vector<int>(target + 1, -1));
+        // return rec(nums, dp, 0, target);
+
+        vector<int> dp(target + 1, -1);
+        return rec2(nums, target, dp);
+    }
+    int table(vector<int>& nums, int target) {
+        vector<unsigned int> dp(target + 1, 0);
+        dp[0] = 1; // Base case: 1 way to make sum 0 (pick nothing)
+
+        for (int i = 1; i <= target; ++i) {
+            for (int num : nums) {
+                if (i - num >= 0) {
+                    dp[i] += dp[i - num];
+                }
+            }
+        }
+
+        return dp[target];
+    }
     int combinationSum4(vector<int>& nums, int target) {
-        vector<vector<int>> dp(nums.size(), vector<int>(target + 1, -1));
-        return rec(nums, dp, 0, target);
+        return mem(nums, target);
+        // return table(nums, target);
     }
 };
