54859461b1854814f115c4488ea963f9a68fcae1
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-07-08 19:16:29 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (82.34%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -18,12 +18,29 @@ public:
         }
         return dp[start][end] = rec(s, dp, uset, start, end + 1);
     }
-    bool mem(string& s, vector<string>& wordDict) {
+    bool mem1(string& s, vector<string>& wordDict) {
         vector<vector<int>> dp(s.size(), vector<int>(s.size(), -1));
         unordered_set<string> uset(wordDict.begin(), wordDict.end());
         return rec(s, dp, uset, 0, 0);
     }
+    bool rec2(const string& s, const vector<string>& wordDict, int idx, vector<int>& memo) {
+        if (idx == s.size()) return true;
+        if (memo[idx] != -1) return memo[idx];
+        for (int i = 0, n = wordDict.size(); i < n; ++i) {
+            int len = wordDict[i].size();
+            if (idx + len <= s.size() && s.compare(idx, len, wordDict[i]) == 0) {
+                if (rec2(s, wordDict, idx + len, memo)) return memo[idx] = true;
+            }
+        }
+        return memo[idx] = false;
+    }
+
+    bool mem2(string& s, vector<string>& wordDict) {
+        vector<int> memo(s.size(), -1);
+        return rec2(s, wordDict, 0, memo);
+    }
     bool wordBreak(string s, vector<string>& wordDict) {
-        return mem(s, wordDict);
+        // return mem(s, wordDict);
+        return mem2(s, wordDict);
     }
 };
