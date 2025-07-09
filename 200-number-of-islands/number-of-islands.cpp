b3bd911513dfb0a1f5edf06c245f76bdc42696d2
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-07-09 21:19:43 +0530

    Time: 26 ms (65.12%) | Memory: 17.8 MB (46.62%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,3 +1,38 @@
+
+class DSU {
+public:
+    vector<int> parent;
+    vector<int> size;
+    DSU(int n) {
+        parent.resize(n);
+        size.resize(n, 1);
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
+        }
+    }
+    int findParent(int node) {
+        if (parent[node] == node) {
+            return node;
+        }
+        return parent[node] = findParent(parent[node]);
+    }
+    void unionn(int u, int v) {
+        int ul_u = findParent(u);
+        int ul_v = findParent(v);
+
+        if (ul_u == ul_v) {
+            return;
+        }
+        if (size[ul_u] > size[ul_v]) {
+            parent[ul_v] = ul_u;
+            size[ul_u] += size[ul_v];
+        } else {
+            parent[ul_u] = ul_v;
+            size[ul_v] += size[ul_u];
+        }
+    }
+};
+
 class Solution {
 public:
     vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
@@ -71,8 +106,46 @@ public:
         }
         return count;
     }
-    int numIslands(vector<vector<char>>& grid) { 
-        // return doDFS(grid); 
-        return doBFS(grid);
+    int doDSU(vector<vector<char>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        DSU dsu(m * n);
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == '0')
+                    continue;
+
+                int u = i * n + j;
+
+                for (auto [dr, dc] : directions) {
+                    int ni = i + dr;
+                    int nj = j + dc;
+
+                    if (ni < 0 || nj < 0 || ni >= m || nj >= n)
+                        continue;
+                    if (grid[ni][nj] == '0')
+                        continue;
+
+                    int v = ni * n + nj;
+                    dsu.unionn(u, v);
+                }
+            }
+        }
+
+        int count = 0;
+        for(int i = 0; i < m * n; ++i) {
+            int row = i / n;
+            int col = i % n;
+            if(i == dsu.findParent(i) && grid[row][col] == '1') {
+                count++;
+            }
+        }
+        return count;
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        // return doDFS(grid);
+        // return doBFS(grid);
+        return doDSU(grid);
     }
 };
