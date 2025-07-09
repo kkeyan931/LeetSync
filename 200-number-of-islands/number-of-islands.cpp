9e9c41401c79490479391b1b49cd16ab54467655
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-07-09 20:04:29 +0530

    Time: 29 ms (45.70%) | Memory: 23.1 MB (36.05%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    vector<pair<int,int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    void dfs(vector<vector<char>> &grid, int row, int col) {
+    vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    void dfs(vector<vector<char>>& grid, int row, int col) {
         grid[row][col] = '0';
 
-        for(auto [dr, dc] : directions) {
+        for (auto [dr, dc] : directions) {
             int nr = row + dr;
             int nc = col + dc;
 
-            if(nr < 0 || nc < 0 || nr >= grid.size() || nc >= grid[0].size()) continue;
+            if (nr < 0 || nc < 0 || nr >= grid.size() || nc >= grid[0].size())
+                continue;
 
-            if(grid[nr][nc] == '0') continue;
+            if (grid[nr][nc] == '0')
+                continue;
 
             dfs(grid, nr, nc);
         }
     }
-    int doDFS(vector<vector<char>> &grid) {
+    int doDFS(vector<vector<char>>& grid) {
         int count = 0;
 
-        for(int i = 0; i < grid.size(); ++i) {
-            for(int j = 0; j < grid[0].size(); ++j) {
-                if(grid[i][j] == '1') {
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[0].size(); ++j) {
+                if (grid[i][j] == '1') {
                     count++;
                     dfs(grid, i, j);
                 }
@@ -28,7 +30,49 @@ public:
         }
         return count;
     }
-    int numIslands(vector<vector<char>>& grid) {
-        return doDFS(grid);
+    void bfs(vector<vector<char>>& grid, int row, int col) {
+
+        grid[row][col] = '0';
+
+        queue<pair<int, int>> q;
+        q.push({row, col});
+
+        while (!q.empty()) {
+            auto [cr, cc] = q.front();
+            q.pop();
+
+            for (auto [dr, dc] : directions) {
+                int nr = cr + dr;
+                int nc = cc + dc;
+
+                if (nr < 0 || nc < 0 || nr >= grid.size() ||
+                    nc >= grid[0].size())
+                    continue;
+
+                if (grid[nr][nc] == '0')
+                    continue;
+
+                grid[nr][nc] = '0';
+
+                q.push({nr, nc});
+            }
+        }
+    }
+    int doBFS(vector<vector<char>>& grid) {
+        int count = 0;
+
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[0].size(); ++j) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    bfs(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+    int numIslands(vector<vector<char>>& grid) { 
+        // return doDFS(grid); 
+        return doBFS(grid);
     }
 };
