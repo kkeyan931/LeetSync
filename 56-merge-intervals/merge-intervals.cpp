eb86e4641abbbd95b20018291fe907de50349a5f
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-07-10 08:14:25 +0530

    Time: 7 ms (41.94%) | Memory: 23.8 MB (80.10%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    static bool comp(vector<int> &a, vector<int> &b) {
-        if(a[0]<b[0]) return true;
-        if(a[0]>b[0]) return false;
-        if(a[0]==b[0]) return a[1]<b[1];
-        return a[1]<b[1];
+    static bool comp(const vector<int> &a, const vector<int> &b) {
+        if(a[0] == b[0]) {
+            return a[1] < b[1];
+        }
+        return a[0] < b[0];
     }
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
         sort(intervals.begin(), intervals.end(), comp);
 
-        int n = intervals.size();
-        if(n==1) return intervals;
-
         vector<vector<int>> result;
-        result.push_back(intervals[0]);
 
-        for(int i=1;i<n;++i) {
-            auto &back = result.back();
-            auto &current = intervals[i];
-
-            if(back[1]>=current[0]) {
-                back[1] = back[1] > current[1] ? back[1] : current[1];
+        for(auto &interval : intervals) {
+            if(result.empty() || result.back()[1] < interval[0]) {
+                result.push_back(interval);
             } else {
-                result.push_back(current);
+                result.back()[1] = max(result.back()[1], interval[1]);
             }
         }
         return result;
