0374ac26f3f3a7e50705cc09dd046e49256d5026
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-07-11 17:09:47 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (75.88%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,47 +1,29 @@
 class Solution {
 public:
-    bool binarySearch(vector<vector<int>>& matrix, int target) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-
-        int start = 0, end = (n*m) -1;
-
-        while(start<=end) {
-            int mid = (start + end) / 2;
-
-            int row = mid/m;
-            int column = mid % m;
-
-            if(matrix[row][column] == target) return true;
-
-            if(target < matrix[row][column]) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
-        }
-        return false;
-    }
-
-    bool travarseAndEliminate(vector<vector<int>>& matrix, int target) {
-        int noOfRows = matrix.size(); // row
-        int noOfColumns = matrix[0].size(); // column
-
-        int row = 0, column = noOfColumns-1;
-
-        while(row < noOfRows && column >= 0) {
-            if(matrix[row][column] == target) return true;
-            
-            if(target < matrix[row][column]) {
-                column--;
-            } else {
-                row++;
-            }
-        }
-        return false;
-    }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        return binarySearch(matrix, target);
-        // return travarseAndEliminate(matrix, target);
+
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int low = 0;
+        int high = (m * n) - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            int row = mid / n;
+            int col = mid % n;
+
+            if (matrix[row][col] == target) {
+                return true;
+            }
+            if (matrix[row][col] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return false;
     }
 };
