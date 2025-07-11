d873dd6b4100ce74365de328f317dd73cb27766b
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-07-11 17:18:50 +0530

    Time: 41 ms (92.77%) | Memory: 18.7 MB (67.67%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -1,62 +1,23 @@
 class Solution {
 public:
-    bool binarySearch(vector<int> &arr, int target) {
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+        int m = matrix.size();
+        int n = matrix[0].size();
 
-        int n = arr.size();
+        int i = 0;
+        int j = n - 1;
 
-        int start = 0, end = n-1;
-
-        while(start<=end) {
-            int mid = (start+end) / 2;
-
-            if(arr[mid] == target) return true;
-
-            if(arr[mid]>target) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
-        }
-        return false;
-    }
-    bool useBinarySearch(vector<vector<int>>& matrix, int target) {
-        int noOfRows = matrix.size();
-        int noOfColumns = matrix[0].size();
-
-        for(int row=0; row<noOfRows; ++row) {
-            if(matrix[row][0]<=target && matrix[row][noOfColumns -1]>=target) {
-                if(binarySearch(matrix[row], target)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-    bool traverseAndEliminate(vector<vector<int>>& matrix, int target) {
-
-        int noOfRows = matrix.size();
-        int noOfColumns = matrix[0].size();
-
-        int row = 0, column = noOfColumns - 1;
-
-        while(row<noOfRows && column>=0) {
-            int element = matrix[row][column];
-
-            if(element == target) {
+        while(i < m && j >= 0) {
+            if(matrix[i][j] == target) {
                 return true;
             }
-
-            if(matrix[row][column] > target) {
-                column--;
-            } else { 
-                row++;
+            if(target <= matrix[i][j] && target >= matrix[i][0]) {
+                j--;
+            } else {
+                i++;
             }
         }
         return false;
-    }
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        // return useBinarySearch(matrix, target);
-
-        return traverseAndEliminate(matrix, target);
     }
 };
