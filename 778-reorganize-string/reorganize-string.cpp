238e85bea5085cad4a0ad9250fb97c2f6854c026
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-07-12 10:48:49 +0530

    Time: 4 ms (5.14%) | Memory: 10.6 MB (5.08%) - LeetSync

--- 778-reorganize-string/reorganize-string.cpp
+++ 778-reorganize-string/reorganize-string.cpp
@@ -1,37 +1,45 @@
 class Solution {
 public:
-    string reorganizeString(string s) {
+    string solution1(string s) {
+        priority_queue<vector<int>> pq;
 
-        priority_queue<pair<int,char>> pq;
-        
-        unordered_map<char,int> umap;
+        unordered_map<char, int> umap;
 
-        for(auto ch : s) {
+        for (auto ch : s) {
             umap[ch]++;
         }
 
         int maxi = 1;
-        for(auto [key, count] : umap) {
+        for (auto [key, count] : umap) {
             pq.push({count, key});
             maxi = max(maxi, count);
         }
 
-        if(maxi > (s.size() + 1) / 2) {
+        if (maxi > (s.size() + 1) / 2) {
             return "";
         }
-        int index = 0;
-        while(!pq.empty()) {
-            auto [count, ch] = pq.top();
+
+        string result = "";
+        while (!pq.empty()) {
+            auto first = pq.top();
             pq.pop();
-            
-            while(count--) {
-                s[index] = ch;
-                index += 2;
-                if(index >= s.size()) {
-                    index = 1;
+
+            if (result.empty() || first[1] != result.back()) {
+                result += char(first[1]);
+                if (--first[0] > 0) {
+                    pq.push(first);
                 }
+            } else {
+                auto second = pq.top();
+                pq.pop();
+                result += char(second[1]);
+                if (--second[0] > 0) {
+                    pq.push(second);
+                }
+                pq.push(first);
             }
         }
-        return s;
+        return result;
     }
+    string reorganizeString(string s) { return solution1(s); }
 };
