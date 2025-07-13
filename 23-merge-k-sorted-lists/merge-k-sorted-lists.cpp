1c2f39a964718a59a338cf7cb795b860582e3387
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-07-13 16:59:12 +0530

    Time: 219 ms (5.06%) | Memory: 18.6 MB (51.56%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        if(l1 == nullptr) {
+            return l2;
+        }
+        if(l2 == nullptr) {
+            return l1;
+        }
+        ListNode* ptr1 = l1;
+        ListNode* ptr2 = l2;
+
+        ListNode* temp = new ListNode(-1);
+        ListNode* tempHead = temp;
+
+        while(ptr1 && ptr2) {
+            if(ptr1->val < ptr2->val) {
+                temp->next = ptr1;
+                ptr1 = ptr1->next;
+            } else {
+                temp->next = ptr2;
+                ptr2 = ptr2->next;
+            }
+            temp = temp->next;
+        }
+        while(ptr1) {
+            temp->next = ptr1;
+            ptr1 = ptr1->next;
+            temp = temp->next;
+        }
+        while(ptr2) {
+            temp->next = ptr2;
+            ptr2 = ptr2->next;
+            temp = temp->next;
+        }
+        return tempHead->next;
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        
+        ListNode* temp = new ListNode(INT_MIN);
+        ListNode* tempHead = temp;
+        for(ListNode* list : lists) {
+            temp = merge(temp, list);
+        }
+        return tempHead->next;
+    }
+};
