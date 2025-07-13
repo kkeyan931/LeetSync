ddd2803ba4fa26701c40b9e918137595c89b2c9a
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-07-13 18:40:40 +0530

    Time: 58 ms (95.49%) | Memory: 36.7 MB (86.63%) - LeetSync

---
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+
+        int n = nums.size();
+
+        vector<int> pointers(n, 0);
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>,
+                       greater<pair<int, int>>>
+            pq;
+
+        vector<int> result(2);
+
+        int mini = INT_MAX;
+        int maxi = INT_MIN;
+        for (int i = 0; i < n; ++i) {
+            pq.push({nums[i][0], i});
+            mini = min(mini, nums[i][0]);
+            maxi = max(maxi, nums[i][0]);
+        }
+        result[0] = mini;
+        result[1] = maxi;
+
+        while (pq.size() == n) {
+            auto [_, index] = pq.top();
+            pq.pop();
+
+            pointers[index]++;
+
+            if (pointers[index] == nums[index].size())
+                break;
+
+            int next = nums[index][pointers[index]];
+
+            pq.push({next, index});
+
+            mini = pq.top().first;
+            maxi = max(next, maxi);
+
+            if (maxi - mini < result[1] - result[0]) {
+                result[0] = mini;
+                result[1] = maxi;
+            }
+        }
+        return result;
+    }
+};
