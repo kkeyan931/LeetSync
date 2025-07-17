24691fc067435ce9146663280f72a7005f4e0950
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-07-17 08:43:34 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (31.26%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        
+    int sol1(vector<int> &height) {
         int n = height.size();
-
         vector<int> a(n);
         vector<int> b(n);
-        
+
         a[0] = height[0];
-        for(int i = 1; i < n; ++i) {
-            a[i] = max(height[i], a[i-1]);
+        for (int i = 1; i < n; ++i) {
+            a[i] = max(height[i], a[i - 1]);
         }
 
         b[n - 1] = height[n - 1];
-        for(int i = n - 2; i >=0; --i) {
-            b[i] = max(height[i], b[i+1]);
+        for (int i = n - 2; i >= 0; --i) {
+            b[i] = max(height[i], b[i + 1]);
         }
 
         int result = 0;
-        for(int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i) {
             result += max(min(a[i], b[i]) - height[i], 0);
         }
         return result;
+    }
+    int sol2(vector<int> &height) {
+        return 0;
+    }
+    int trap(vector<int>& height) {
+        return sol1(height);
     }
 };
