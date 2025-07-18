49e260bb65bb9ee893143ca8d0ff700c46a112cc
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-07-18 09:11:27 +0530

    Time: 3 ms (37.82%) | Memory: 40.2 MB (58.04%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,62 +1,29 @@
 class Solution {
 public:
-    vector<int> optimal(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> result(n);
+    vector<int> productExceptSelf(vector<int>& nums) {
         
-        result[0]=nums[0];
-        for(int i=1;i<n;++i){
-            result[i]=result[i-1]*nums[i];
-        }
-        
-        int rpr=1;
-        for(int i=n-1;i>0;--i){
-            result[i]=result[i-1] * rpr;
-            rpr=rpr * nums[i];
-        }
-        result[0]=rpr;
-        
-        return result;
-    }
-    vector<int> naive(vector<int>& nums) {
         int n = nums.size();
+
         vector<int> result(n, 0);
 
-        int zeroCount = 0;
+        result[0] = nums[0];
 
-        for (int i = 0; i < n; ++i) {
-            if (nums[i] == 0) {
-                zeroCount++;
-            }
+        // prefix[i - 1] * suffix[i + 1] = product of the array except self
+
+        // prefix of the array
+        for(int i = 1; i < n; ++i) {
+            result[i] = result[i - 1] * nums[i];
         }
-        if (zeroCount > 1) {
-            return result;
-        }
-        if (zeroCount == 1) {
-            int product = 1;
-            int index = 0;
-            for (int i = 0; i < n; ++i) {
-                if (nums[i] != 0) {
-                    product *= nums[i];
-                }
-                if (nums[i] == 0) {
-                    index = i;
-                }
-            }
-            result[index] = product;
-            return result;
+        int suffix = 1;
+
+        // suffix of the array
+        for(int i = n - 1; i > 0; --i) {
+            result[i] = result[i - 1] * suffix;
+            suffix = suffix * nums[i];
         }
 
-        int product = 1;
-        for (int i = 0; i < n; ++i) {
-            product *= nums[i];
-        }
-        for (int i = 0; i < n; ++i) {
-            result[i] = product / nums[i];
-        }
+        result[0] = suffix;
+
         return result;
-    }
-    vector<int> productExceptSelf(vector<int>& nums) {
-        return optimal(nums);
     }
 };
