5b8e27345438947cd0a751ddef2651967f04f071
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-07-18 16:02:37 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (39.04%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -2,15 +2,32 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
 
-        int it = n+m-1;
-        int ptr1=m-1;
-        int ptr2=n-1;
+        int ptr1 = m - 1;
+        int ptr2 = n - 1;
 
-        while(ptr1>=0 && ptr2>=0) {
-            if(nums1[ptr1]>nums2[ptr2]) nums1[it--] = nums1[ptr1--];
-            else nums1[it--] = nums2[ptr2--]; 
+        int k = (m + n) - 1;
+
+        while (ptr1 >= 0 && ptr2 >= 0) {
+            if (nums1[ptr1] > nums2[ptr2]) {
+                nums1[k] = nums1[ptr1];
+                ptr1--;
+            } else {
+                nums1[k] = nums2[ptr2];
+                ptr2--;
+            }
+            k--;
         }
-        while(ptr1>=0) nums1[it--] = nums1[ptr1--];
-        while(ptr2>=0) nums1[it--] = nums2[ptr2--];
+
+        while (ptr1 >= 0) {
+            nums1[k] = nums1[ptr1];
+            ptr1--;
+            k--;
+        }
+
+        while (ptr2 >= 0) {
+            nums1[k] = nums2[ptr2];
+            ptr2--;
+            k--;
+        }
     }
 };
