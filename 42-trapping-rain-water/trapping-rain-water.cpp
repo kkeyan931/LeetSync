21b6182e9ead7d3204287f45064bd48e987e433a
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-07-18 16:18:53 +0530

    Time: 0 ms (100.00%) | Memory: 25.9 MB (93.30%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int sol1(vector<int> &height) {
+    int sol1(vector<int>& height) {
         int n = height.size();
         vector<int> a(n);
         vector<int> b(n);
@@ -21,10 +21,30 @@ public:
         }
         return result;
     }
-    int sol2(vector<int> &height) {
-        return 0;
+    int sol2(vector<int>& height) {
+
+        int left = 0;
+        int right = height.size() - 1;
+
+        int result = 0;
+
+        int left_max = 0;
+        int right_max = 0;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                height[left] >= left_max ? (left_max = height[left])
+                                         : result += (left_max - height[left]);
+                left++;
+            } else {
+                height[right] >= right_max ? (right_max = height[right])
+                                           : result += (right_max - height[right]);
+                right--;
+            }
+        }
+        return result;
     }
-    int trap(vector<int>& height) {
-        return sol1(height);
+    int trap(vector<int>& height) { 
+        // return sol1(height); 
+        return sol2(height);
     }
 };
