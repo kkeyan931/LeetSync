9fc4474be8c6445007c7bec1f301eda9d1261cf0
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-07-18 08:39:41 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (78.77%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,66 +1,18 @@
 class Solution {
 public:
-    int getNumIndex(vector<int> &nums, int i)
-    {
-        int n = nums.size();
-        while (i < n)
-        {
-            if (nums[i] != INT_MIN)
-                return i;
-            i++;
-        }
-        return -1;
-    }
-    void swap(vector<int> &nums, int i, int j)
-    {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-    int removeDuplicates(vector<int> &nums)
-    {
+    int removeDuplicates(vector<int>& nums) {
+        
+        int last = nums[0];
+        int ptr1 = 1, ptr2 = 1;
 
-        int n = nums.size();
-        if (n <= 1)
-            return n;
-
-        int i = 0, j = 1;
-
-        int k = 1;
-        while (j < n)
-        {
-            if (nums[i] == nums[j])
-            {
-                nums[j] = INT_MIN;
-                j++;
+        while(ptr2 < nums.size()) {
+            if(nums[ptr2] != last) {
+                nums[ptr1] = nums[ptr2];
+                ptr1++;
+                last = nums[ptr2];
             }
-            else
-            {
-                k++;
-                i = j;
-                j++;
-            }
+            ptr2++;
         }
-
-        i = 0;
-        while (i < n)
-        {
-            if (nums[i] == INT_MIN)
-            {
-                int j = getNumIndex(nums, i + 1);
-                if (j != -1)
-                {
-                    swap(nums, i, j);
-                    i++;
-                    continue;
-                }
-                else
-                {
-                    break;
-                }
-            }
-            i++;
-        }
-        return k;
+        return ptr1;
     }
 };
