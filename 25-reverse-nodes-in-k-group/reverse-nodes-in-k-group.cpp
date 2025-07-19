869d870f606d510ba6fbd0a8d138de59dc24b1df
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-07-19 14:34:13 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (71.49%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -11,37 +11,44 @@
 class Solution {
 public:
     int getSize(ListNode* head) {
+        int count = 0;
         ListNode* ptr = head;
 
-        int count = 0;
-        while(ptr) {
+        while (ptr) {
             ptr = ptr->next;
             count++;
         }
         return count;
     }
-    ListNode* rec(ListNode* head, int n, int k) {
-        if(n < k) {
+
+    ListNode* reverse(ListNode* head, int n, int k) {
+        if (n < k) {
             return head;
         }
 
         ListNode* prev = nullptr;
         ListNode* next = head;
-
-        int count = k; 
-        while(next && count--) {
+        int count = k;
+        while (next && count--) {
             ListNode* nextNext = next->next;
             next->next = prev;
             prev = next;
             next = nextNext;
         }
-        if(next) {
-            head->next = rec(next, n - k, k);
+
+        if (next) {
+            head->next = reverse(next, n - k, k);
         }
+
         return prev;
-    } 
+    }
     ListNode* reverseKGroup(ListNode* head, int k) {
+
+        if (k == 1) {
+            return head;
+        }
         int n = getSize(head);
-        return rec(head, n, k);
+
+        return reverse(head, n, k);
     }
 };
