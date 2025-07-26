07558e6e4d9d299b5c44ad407942c33ba6c25c7e
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-07-26 18:52:42 +0530

    Time: 12 ms (52.86%) | Memory: 58.1 MB (77.95%) - LeetSync

--- 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.cpp
+++ 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
-
+        /*
+        1. Be greedy and use ladders for each gap, but also maintain a priority
+        queue for each gap.
+        2. When you run out of ladders, try to see if the smallest used ladder
+        is smaller than current gap; this way you switch them and replace the
+        smallest one with bricks.
+        3. If out of ladders, unable to swap and repalce with bricks, stop
+        there.
+        */
         priority_queue<int, vector<int>, greater<int>> pq;
 
         int n = heights.size();
         int i = 0;
-        while (i < n - 1) {
-            if (heights[i + 1] <= heights[i]) {
-                i++;
-                continue;
-            }
-            if (ladders > 0) {
-                ladders--;
-                pq.push(heights[i + 1] - heights[i]);
-                i++;
-                continue;
-            }
-            int gap = heights[i + 1] - heights[i];
-            if (!pq.empty()) {
-                int top = pq.top();
 
-                if (gap > top && bricks >= top) {
-                    bricks = bricks - top;
-                    pq.pop();
-                    pq.push(gap);
-                    i++;
-                    continue;
-                }
-            }
-            if (gap <= bricks) {
-                bricks = bricks - gap;
-                i++;
+        for (int i = 0; i < n - 1; ++i) {
+            int climb = heights[i + 1] - heights[i];
+
+            if (climb <= 0) {
                 continue;
             }
-            return i;
+
+            pq.push(climb);
+
+            if (pq.size() <= ladders) {
+                continue;
+            }
+            int top = pq.top();
+            pq.pop();
+            bricks = bricks - top;
+
+            if (bricks < 0) {
+                return i;
+            }
         }
-        return i;
+        return n - 1;
     }
 };
