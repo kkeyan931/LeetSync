79c078afcc04924eb046e363130dc4f8c6d3d02d
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-07-26 15:27:51 +0530

    Time: 0 ms (100.00%) | Memory: 27.8 MB (76.17%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -21,11 +21,18 @@ public:
         int l = rec(root->left);
         int r = rec(root->right);
         
+        /*
+            1. consider only root.
+            2. consider the root and left
+            3. consider the root and right
+            4. consider the root, left and right
+        */
         maxi = max(root->val, maxi);
         maxi = max(root->val + l, maxi);
         maxi = max(root->val + r, maxi);
         maxi = max(root->val + l + r, maxi);
 
+        // consider only the root and left and right max
         return max(root->val, max(root->val + l, root->val + r));
     }
     int maxPathSum(TreeNode* root) {
