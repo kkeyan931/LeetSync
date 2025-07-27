4e572a5d7e5b3d4c5f8def884ff09ba47d0feb40
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-07-27 18:09:06 +0530

    Time: 1 ms (85.05%) | Memory: 13.7 MB (76.31%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     void dfs(vector<vector<int>>& graph, int node, vector<vector<int>>& result,
              vector<int>& temp) {
-        temp.push_back(node);
 
         if (node == graph.size() - 1) {
             result.push_back(temp);
-            temp.pop_back();
             return;
         }
         for (auto nextNode : graph[node]) {
+            temp.push_back(nextNode);
             dfs(graph, nextNode, result, temp);
+            temp.pop_back();
         }
-        temp.pop_back();
     }
     vector<vector<int>> doDFS(vector<vector<int>>& graph) {
         vector<vector<int>> result;
         vector<int> temp;
+        temp.push_back(0);
         dfs(graph, 0, result, temp);
 
         return result;
