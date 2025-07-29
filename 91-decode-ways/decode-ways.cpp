d77675eb1158bb96eaeb915b5903238d65d77a13
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-07-29 14:52:03 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (20.03%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,55 +1,33 @@
 class Solution {
 public:
-    int rec(string& s, vector<int>& dp, int index) {
-        if (index >= s.size()) {
+    int rec(string& s, vector<int> &dp, int index) {
+
+        if (index == s.size()) {
             return 1;
         }
-        if (s[index] == '0') {
-            return 0;
-        }
-        if (dp[index] != -1) {
+
+        if(dp[index] != -1) {
             return dp[index];
         }
-        int one = rec(s, dp, index + 1);
-        int two = 0;
-        if (index < s.size() - 1) {
-            string numStr = std::string(1, s[index]) + s[index + 1];
-            int num = stoi(numStr);
-            if (num >= 10 && num <= 26) {
-                two = rec(s, dp, index + 2);
+
+        int count = 0;
+        if (index < s.size()) {
+            char str = s[index];
+            if (str >= '1' && str <= '9') {
+                count += rec(s, dp, index + 1);
             }
         }
-        return dp[index] = one + two;
-    }
-    int mem(string& s) {
-        vector<int> dp(s.size(), -1);
-        return rec(s, dp, 0);
-    }
-    int table(string& s) {
-        if (s[0] == '0') {
-            return 0;
-        }
+        if (index + 1 < s.size()) {
+            string str = s.substr(index, 2);
 
-        int n = s.length();
-        vector<int> dp(n + 1, 0);
-        dp[0] = dp[1] = 1;
-
-        for (int i = 2; i <= n; i++) {
-            int one = s[i - 1] - '0';
-            int two = stoi(s.substr(i - 2, 2));
-
-            if (1 <= one && one <= 9) {
-                dp[i] += dp[i - 1];
-            }
-            if (10 <= two && two <= 26) {
-                dp[i] += dp[i - 2];
+            if (s[0] != '0' && stoi(str) >= 10 && stoi(str) <= 26) {
+                count += rec(s, dp, index + 2);
             }
         }
-
-        return dp[n];
+        return dp[index] = count;
     }
     int numDecodings(string s) {
-        // return mem(s);
-        return table(s);
+        vector<int> dp(s.size() + 1, -1);
+        return rec(s, dp, 0); 
     }
 };
