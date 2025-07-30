d3881545c2d34097697693e40415bc1f4eb7b957
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-07-30 16:07:41 +0530

    Time: 178 ms (18.16%) | Memory: 110.5 MB (40.12%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
-    bool rec(vector<int> &nums, vector<vector<int>> &dp, int index, int target) {
-        if(target == 0) {
+    int rec(vector<int>& nums, vector<vector<int>>& dp, int index, int target) {
+        if (target == 0) {
             return true;
         }
-        if(index == nums.size()) {
+        if (target < 0) {
             return false;
         }
-        if(target < 0) {
+        if (index == nums.size()) {
             return false;
         }
 
-        if(dp[index][target] != -1) {
+        if (dp[index][target] != -1) {
             return dp[index][target];
         }
 
-        bool take = rec(nums, dp, index + 1, target - nums[index]);
-        bool notTake = rec(nums, dp, index + 1, target);
+        int take = rec(nums, dp, index + 1, target - nums[index]);
+        int notTake = rec(nums, dp, index + 1, target);
 
         return dp[index][target] = take || notTake;
     }
-    bool mem(vector<int> &nums) {
+    bool mem(vector<int>& nums) {
         int total = accumulate(nums.begin(), nums.end(), 0);
 
-        if(total % 2 != 0) {
+        if (total % 2 != 0) {
             return false;
         }
-        int target = total / 2;
-
-        vector<vector<int>> dp(nums.size(), vector<int>(target + 1, -1));
-
-        return rec(nums, dp, 0, target);
+        vector<vector<int>> dp(nums.size(), vector<int>(total / 2 + 1, -1));
+        return rec(nums, dp, 0, total / 2);
     }
-    bool canPartition(vector<int>& nums) {
-        return mem(nums);
-    }
+    bool canPartition(vector<int>& nums) { return mem(nums); }
 };
