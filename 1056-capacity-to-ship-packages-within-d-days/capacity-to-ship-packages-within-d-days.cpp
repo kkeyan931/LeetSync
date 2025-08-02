39f3b09ef6e19b7868bc913d76a48ecd8f031682
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-08-02 09:13:35 +0530

    Time: 9 ms (78.55%) | Memory: 35.1 MB (9.26%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool canShip(vector<int>& weights, long long capacity, int days) {
+
+        int countDays = 1, weightAccumulator = 0;
+        for (auto& weight : weights) {
+            if (weightAccumulator + weight <= capacity) {
+                weightAccumulator += weight;
+            } else {
+                countDays++;
+                weightAccumulator = weight;
+            }
+            if (weightAccumulator > capacity)
+                return false;
+        }
+
+        return countDays <= days;
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+
+        long long sum = accumulate(weights.begin(), weights.end(), 0);
+        int max = *max_element(weights.begin(), weights.end());
+        if (days == 1) {
+            return sum;
+        }
+
+        long long start = max, end = sum;
+
+        long long minCapacity = sum;
+        while (start <= end) {
+            long long mid = (start + end) / 2;
+
+            if (canShip(weights, mid, days)) {
+                minCapacity = min(minCapacity, mid);
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return minCapacity;
+    }
+};
