a7044e94c0a95a3755a59babc237a9696f129993
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-08-02 18:17:06 +0530

    Time: 236 ms (9.48%) | Memory: 166.5 MB (19.68%) - LeetSync

---
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int getNextIndex(vector<vector<int>>& events, int target) {
+        int low = 0;
+        int high = events.size() - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (target >= events[mid][0]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+    int rec(vector<vector<int>>& events, vector<vector<int>> &dp, int index, int k) {
+        if (index == events.size()) {
+            return 0;
+        }
+        if (k == 0) {
+            return 0;
+        }
+        if(dp[index][k] != -1) {
+            return dp[index][k];
+        }
+
+        int pick = events[index][2] +
+                   rec(events, dp, getNextIndex(events, events[index][1]), k - 1);
+        int notPick = rec(events, dp, index + 1, k);
+
+        return dp[index][k] = max(pick, notPick);
+    }
+    int mem(vector<vector<int>>& events, int k) {
+        sort(events.begin(), events.end());
+
+        vector<vector<int>> dp(events.size(), vector<int>(k + 1, -1));
+        return rec(events, dp, 0, k);
+    }
+    int maxValue(vector<vector<int>>& events, int k) { return mem(events, k); }
+};
