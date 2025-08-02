ec365d8fd07403416528b9dc3ac7bcfc242b9114
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-08-02 16:02:17 +0530

    Time: 202 ms (42.91%) | Memory: 12.5 MB (91.38%) - LeetSync

---
+++ 204-count-primes/count-primes.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if (n <= 2) {
+            return 0;
+        }
+
+        vector<bool> numbers(n, true);
+        for (int p = 2; p <= sqrt(n); ++p) {
+            if (numbers[p]) {
+                for (int j = p * p; j < n; j += p) {
+                    numbers[j] = false;
+                }
+            }
+        }
+
+        int numberOfPrimes = 0;
+        for (int i = 2; i < n; i++) {
+            if (numbers[i]) {
+                ++numberOfPrimes;
+            }
+        }
+
+        return numberOfPrimes;
+    }
+};
