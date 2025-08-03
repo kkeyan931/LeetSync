2c6c3d3931a2df24aa06d98b044fc2d7fec9fcea
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-08-03 22:00:44 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (88.68%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -3,18 +3,18 @@ public:
     vector<vector<int>> generate(int numRows) {
 
         vector<vector<int>> result;
-        
-        result.push_back({1});
-        
-        int i = 1;
-        while(i<numRows) {
-            vector<int> innerVec(i+1);
 
-            for(int j=0;j<=i;++j) {
-                if(j==0 || j==i) {
+        result.push_back({1});
+
+        int i = 1;
+        while (i < numRows) {
+            vector<int> innerVec(i + 1);
+
+            for (int j = 0; j <= i; ++j) {
+                if (j == 0 || j == i) {
                     innerVec[j] = 1;
                 } else {
-                    innerVec[j] = result[i-1][j-1] + result[i-1][j];
+                    innerVec[j] = result[i - 1][j - 1] + result[i - 1][j];
                 }
             }
             result.push_back(innerVec);
