fcd0a8fc78e491635fb46a29b83e357c3f93aaf0
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-08-03 17:39:32 +0530

    Time: 15 ms (46.05%) | Memory: 19 MB (81.47%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -18,23 +18,17 @@ public:
 
 class Solution {
 public:
-    Node* connect(Node* root) {
-
-        if(root == nullptr) {
+    Node* bfs(Node* root) {
+        if (root == nullptr) {
             return root;
         }
-
         queue<Node*> q;
-
         q.push(root);
 
         while (!q.empty()) {
-
             int size = q.size();
-
             Node* prev = nullptr;
             while (size--) {
-
                 auto node = q.front();
                 q.pop();
                 node->next = prev;
@@ -49,5 +43,37 @@ public:
             }
         }
         return root;
+    }
+    Node* optimal(Node* root) {
+        if (root == nullptr) {
+            return root;
+        }
+        // Start with the root node. There are no next pointers
+        // that need to be set up on the first level
+        Node* leftmost = root;
+        // Once we reach the final level, we are done
+        while (leftmost->left != nullptr) {
+            // Iterate the "linked list" starting from the head
+            // node and using the next pointers, establish the
+            // corresponding links for the next level
+            Node* head = leftmost;
+            while (head != nullptr) {
+                // CONNECTION 1
+                head->left->next = head->right;
+                // CONNECTION 2
+                if (head->next != nullptr) {
+                    head->right->next = head->next->left;
+                }
+                // Progress along the list (nodes on the current level)
+                head = head->next;
+            }
+            // Move onto the next level
+            leftmost = leftmost->left;
+        }
+        return root;
+    }
+    Node* connect(Node* root) {
+        // return bfs(root);
+        return optimal(root);
     }
 };
