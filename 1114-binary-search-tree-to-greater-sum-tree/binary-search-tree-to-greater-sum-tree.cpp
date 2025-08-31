efd5d3fa644fc387645252ada055a2d637d7678e
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-08-31 14:52:38 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (9.11%) - LeetSync

---
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int prev = 0;
+    void dfs(TreeNode* root) {
+        if(root == nullptr) {
+            return;
+        }
+        dfs(root->right);
+        
+        root->val = root->val + prev;
+        prev = root->val;
+
+        dfs(root->left);
+    }
+    TreeNode* bstToGst(TreeNode* root) {
+        dfs(root);
+
+        return root;
+    }
+};
