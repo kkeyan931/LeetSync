ec55197aa8c389c41e104a4cb4fe33f20f884c60
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-08-31 09:27:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (95.96%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,31 +1,43 @@
 class Solution {
 public:
-    void rec(vector<string>& result, unordered_map<char, string>& umap,
-             string& temp, string& digits, int index) {
+    void combinationsBuilder(string& digits, vector<string>& result,
+                             string& builder,
+                             unordered_map<char, string>& digitsMap,
+                             int digitsIterator) {
 
-        if (temp.size() == digits.size()) {
-            result.push_back(temp);
+        if (digitsIterator == digits.size()) {
+            result.push_back(builder);
             return;
         }
 
-        for (int i = index; i < digits.size(); ++i) {
-            for (auto ch : umap[digits[i]]) {
-                temp.push_back(ch);
-                rec(result, umap, temp, digits, i + 1);
-                temp.pop_back();
-            }
+        for (auto character : digitsMap[digits[digitsIterator]]) {
+            builder.push_back(character);
+            combinationsBuilder(digits, result, builder, digitsMap,
+                                digitsIterator + 1);
+            builder.pop_back();
         }
     }
     vector<string> letterCombinations(string digits) {
+
         if (digits.size() == 0) {
             return {};
         }
-        unordered_map<char, string> umap = {
-            {'2', "abc"}, {'3', "def"},  {'4', "ghi"}, {'5', "jkl"},
-            {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}};
+
         vector<string> result;
-        string temp;
-        rec(result, umap, temp, digits, 0);
+        string builder;
+
+        unordered_map<char, string> digitsMap;
+        digitsMap['2'] = "abc";
+        digitsMap['3'] = "def";
+        digitsMap['4'] = "ghi";
+        digitsMap['5'] = "jkl";
+        digitsMap['6'] = "mno";
+        digitsMap['7'] = "pqrs";
+        digitsMap['8'] = "tuv";
+        digitsMap['9'] = "wxyz";
+
+        combinationsBuilder(digits, result, builder, digitsMap, 0);
+
         return result;
     }
 };
