35c927b73eaa0bf5e956e47347a824698781157e
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-08-31 11:26:57 +0530

    Time: 186 ms (92.20%) | Memory: 10.4 MB (86.25%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,67 +1,50 @@
 class Solution {
 public:
-    vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     bool dfs(vector<vector<char>>& board, string& word, int row, int col,
              int index) {
-        int m = board.size();
-        int n = board[0].size();
+
+        int rowSize = board.size();
+        int colSize = board[0].size();
 
         if (index == word.size()) {
             return true;
         }
 
-        char ch = board[row][col];
+        if (row < 0 || col < 0 || row >= rowSize || col >= colSize) {
+            return false;
+        }
 
+        if (board[row][col] == '#') {
+            return false;
+        }
+
+        if (board[row][col] != word[index]) {
+            return false;
+        }
+
+        char ch = board[row][col];
         board[row][col] = '#';
 
-        bool ans = false;
-        for (auto [dr, dc] : directions) {
-            int nr = row + dr;
-            int nc = col + dc;
+        bool found = false;
 
-            if (nr < 0 || nc < 0 || nr >= m || nc >= n) {
-                continue;
-            }
+        found = found || dfs(board, word, row + 1, col, index + 1);
+        found = found || dfs(board, word, row - 1, col, index + 1);
+        found = found || dfs(board, word, row, col + 1, index + 1);
+        found = found || dfs(board, word, row, col - 1, index + 1);
 
-            if (board[nr][nc] == '#') {
-                continue;
-            }
-
-            if (board[nr][nc] != word[index]) {
-                continue;
-            }
-            ans = ans || dfs(board, word, nr, nc, index + 1);
-        }
         board[row][col] = ch;
-        return ans;
+        return found;
     }
+    bool exist(vector<vector<char>>& board, string word) {
 
-    bool doDFS(vector<vector<char>>& board, string& word) {
-        int m = board.size();
-        int n = board[0].size();
-
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (board[i][j] == word[0] && dfs(board, word, i, j, 1)) {
+        for (int rowIndex = 0; rowIndex < board.size(); ++rowIndex) {
+            for (int colIndex = 0; colIndex < board[0].size(); ++colIndex) {
+                if (board[rowIndex][colIndex] == word[0] &&
+                    dfs(board, word, rowIndex, colIndex, 0)) {
                     return true;
                 }
             }
         }
         return false;
-    }
-
-    bool exist(vector<vector<char>>& board, string word) {
-        /*
-            BFS will not work here, because it'll consider all the adjacent as
-           possible solution. which will not explore all the possibilities.
-
-            [["A","B","C","E"],["S","F","E","S"],["A","D","E","E"]]
-            ABCESEEEFS -> expected true
-
-            BFS will fail for the above case.
-
-            Exploring all the paths with backtracking will work.
-        */
-        return doDFS(board, word);
     }
 };
