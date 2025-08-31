a71491cb9f80e597ad7cccf7264650019347877e
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-08-31 10:08:15 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (54.40%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
-    void rec(vector<vector<int>>& result, vector<int>& temp, vector<int>& nums,
-             int index, int target) {
+    void combinationBuilder(vector<int>& candidates,
+                            vector<vector<int>>& result, vector<int>& builder,
+                            int index, int target) {
+
         if (target == 0) {
-            result.push_back(temp);
-            return;
-        }
-        if (target < 0) {
-            return;
-        }
-        if (index == nums.size()) {
+            result.push_back(builder);
             return;
         }
 
-        temp.push_back(nums[index]);
-        rec(result, temp, nums, index, target - nums[index]);
-        temp.pop_back();
-        rec(result, temp, nums, index + 1, target);
+        if (index >= candidates.size()) {
+            return;
+        }
+
+        if (target - candidates[index] >= 0) {
+            builder.push_back(candidates[index]);
+            combinationBuilder(candidates, result, builder, index,
+                               target - candidates[index]);
+            builder.pop_back();
+        }
+        combinationBuilder(candidates, result, builder, index + 1, target);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> result;
-        vector<int> temp;
 
-        rec(result, temp, candidates, 0, target);
+        vector<vector<int>> result;
+        vector<int> builder;
+
+        combinationBuilder(candidates, result, builder, 0, target);
 
         return result;
     }
