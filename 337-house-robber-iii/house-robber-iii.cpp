db2795d1df0e8ba8256c46f9914067485ca766b4
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-09-01 15:39:27 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (67.78%) - LeetSync

--- 337-house-robber-iii/house-robber-iii.cpp
+++ 337-house-robber-iii/house-robber-iii.cpp
@@ -12,25 +12,48 @@
  */
 class Solution {
 public:
-    int rec(TreeNode* root, unordered_map<TreeNode*, int> &memo) {
+    pair<int, int> dfs(TreeNode* root) {
+        if (!root)
+            return {0, 0};
+        auto left = dfs(root->left);
+        auto right = dfs(root->right);
+
+        int include = root->val + left.second + right.second;
+        int exclude =
+            max(left.first, left.second) + max(right.first, right.second);
+
+        return {include, exclude};
+    }
+    int optimal(TreeNode* root) {
+        auto ans = dfs(root);
+        return max(ans.first, ans.second);
+    }
+    int rec(TreeNode* root, unordered_map<TreeNode*, int>& memo) {
         if (root == nullptr) {
             return 0;
         }
-        if(memo.find(root) != memo.end()) {
+        if (memo.find(root) != memo.end()) {
             return memo[root];
         }
-        int include = root->val +
-                      rec(root->left ? root->left->left : root->left, memo) +
-                      rec(root->left ? root->left->right : root->left, memo) +
-                      rec(root->right ? root->right->left : root->right, memo) +
-                      rec(root->right ? root->right->right : root->right, memo);
-
+        int include = root->val;
+        if (root->left) {
+            include +=
+                rec(root->left->left, memo) + rec(root->left->right, memo);
+        }
+        if (root->right) {
+            include +=
+                rec(root->right->left, memo) + rec(root->right->right, memo);
+        }
         int exclude = rec(root->left, memo) + rec(root->right, memo);
 
         return memo[root] = max(include, exclude);
     }
-    int rob(TreeNode* root) { 
+    int better(TreeNode* root) {
         unordered_map<TreeNode*, int> memo;
-        return rec(root, memo); 
+        return rec(root, memo);
+    }
+    int rob(TreeNode* root) {
+        // return better(root);
+        return optimal(root);
     }
 };
