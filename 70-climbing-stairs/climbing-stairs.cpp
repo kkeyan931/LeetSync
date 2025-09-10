7f6630deeb37df7e4f53867b607832a5dc95d1a2
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-09-10 08:24:09 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (87.78%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    int rec(vector<int> &dp, int n) {
-        if(n <= 2) {
+    int rec(vector<int>& dp, int n) {
+        if (n <= 2) {
             return n;
         }
-        if(dp[n] != -1) {
+        if (dp[n] != -1) {
             return dp[n];
         }
         return dp[n] = rec(dp, n - 1) + rec(dp, n - 2);
@@ -15,28 +15,28 @@ public:
     }
     int table(int n) {
 
-        if(n <= 2) {
+        if (n <= 2) {
             return n;
         }
-        
+
         vector<int> dp(n + 1);
         dp[0] = 0;
         dp[1] = 1;
         dp[2] = 2;
 
-        for(int i = 3; i <= n; ++i) {
+        for (int i = 3; i <= n; ++i) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
     }
     int space(int n) {
-        if(n <= 2) {
+        if (n <= 2) {
             return n;
         }
         int a = 1;
         int b = 2;
         int c;
-        for(int i = 3; i <= n; ++i) {
+        for (int i = 3; i <= n; ++i) {
             c = a + b;
             a = b;
             b = c;
