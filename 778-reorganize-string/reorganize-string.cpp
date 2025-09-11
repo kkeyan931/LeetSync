bc188c62c428aec5e86b5c92873ca2a5fa869797
Karthikeyan K <65884897+kkeyan931@users.noreply.github.com>
2025-09-11 17:49:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (48.94%) - LeetSync

--- 778-reorganize-string/reorganize-string.cpp
+++ 778-reorganize-string/reorganize-string.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
-    string reorganizeString(string s) {
-
+    string solution1(string s) {
         int n = s.size();
-
         unordered_map<char, int> umap;
 
         for (auto ch : s) {
@@ -34,5 +32,52 @@ public:
         }
         string str(result.begin(), result.end());
         return str;
+    }
+    string solution2(string inputString) {
+        vector<int> charCounter(26, 0);
+
+        for (char character : inputString) {
+            charCounter[character - 'a']++;
+        }
+
+        int maxCount = 0, maxLetter = 0;
+        for (int index = 0; index < charCounter.size(); index++) {
+            if (charCounter[index] > maxCount) {
+                maxCount = charCounter[index];
+                maxLetter = index;
+            }
+        }
+
+        if (maxCount > (inputString.length() + 1) / 2) {
+            return "";
+        }
+
+        vector<char> result(inputString.size());
+        int resultIterator = 0;
+
+        // Place the most frequent letter
+        while (charCounter[maxLetter] != 0) {
+            result[resultIterator] = char(maxLetter + 'a');
+            resultIterator += 2;
+            charCounter[maxLetter]--;
+        }
+
+        // Place rest of the letters in any order
+        for (int index = 0; index < charCounter.size(); index++) {
+            while (charCounter[index] > 0) {
+                if (resultIterator >= inputString.length()) {
+                    resultIterator = 1;
+                }
+                result[resultIterator] = char(index + 'a');
+                resultIterator += 2;
+                charCounter[index]--;
+            }
+        }
+        string resultString(result.begin(), result.end());
+        return resultString;
+    }
+    string reorganizeString(string s) {
+        // return solution1(s);
+        return solution2(s);
     }
 };
